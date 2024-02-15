80646def5cf7d1dcd40d555cf8d335388bfb6d5d
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-02-16 00:07:10 +0900

    123

--- NY_Git/pythonProject/240215_QUEUE/swea_15126.py
+++ NY_Git/pythonProject/240215_QUEUE/swea_15126.py
@@ -1,28 +1,54 @@
+# T = int(input())
+#
+# for tc in range(1, T+1):
+#     N, M = map(int, input().split())
+#     cheese = list(map(int, input().split()))
+#     pizza_pot = [-1] * N
+#     cheese_lst = [[i+1, cheese[i]] for i in range(len(cheese))]
+#
+#     for _ in range(N):
+#         pizza_pot.pop()
+#         pizza_pot.insert(0, cheese_lst.pop(0))
+#
+#     while len(pizza_pot) > 1:
+#         if len(cheese_lst) != 0:
+#             if pizza_pot[0][-1] == 0:
+#                 pizza_pot.pop(0)
+#                 pizza_pot.insert(0, cheese_lst.pop(0))
+#             else:
+#                 pizza_pot.append([pizza_pot[0][0], pizza_pot[0][-1]//2])
+#                 pizza_pot.pop(0)
+#         else: # cheese에 값이 없을 경우
+#             if pizza_pot[0][-1] == 0:
+#                 pizza_pot.pop(0)
+#             else:
+#                 pizza_pot.append([pizza_pot[0][0], pizza_pot[0][-1]//2])
+#                 pizza_pot.pop(0)
+#
+#     print(f'#{tc} {pizza_pot[0][0]}')
+#
+#
+#
+#
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    cheese = list(map(int, input().split()))
-    pizza_pot = [-1] * N
-    cheese_lst = [[i+1, cheese[i]] for i in range(len(cheese))]
+    arr = list(map(int, input().split()))
 
-    for _ in range(N):
-        pizza_pot.pop()
-        pizza_pot.insert(0, cheese_lst.pop(0))
+    Q = []
+    for i in range(N):
+        Q.append((i + 1, arr[i]))
+    i = N
+    while len(Q) >= 1:
+        num, cheese = Q.pop(0)
+        cheese = cheese // 2
 
-    while len(pizza_pot) == 1:
-        if len(cheese_lst) != 0:
-            if pizza_pot[0][-1] == 0:
-                pizza_pot.pop(0)
-                pizza_pot.insert(0, cheese_lst.pop(0))
-            else:
-                pizza_pot.append([pizza_pot[0][0], pizza_pot[0][-1]//2])
-                pizza_pot.pop(0)
-        else: # cheese에 값이 없을 경우
-            if pizza_pot[0][-1] == 0:
-                pizza_pot.pop(0)
-            else:
-                pizza_pot.append([pizza_pot[0][0], pizza_pot[0][-1]//2])
-                pizza_pot.pop(0)
+        if cheese == 0:
+            if i < M:
+                Q.append((i + 1, arr[i]))
+                i += 1
+        else:
+            Q.append((num, cheese))
 
-    print(f'#{tc} {pizza_pot[0][0]}')
+    print(f'#{tc} {num}')
