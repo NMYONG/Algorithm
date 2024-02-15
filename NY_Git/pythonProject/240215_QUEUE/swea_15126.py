774008406f2e3cbe08f0483342aa7ee01a456c0d
NMYONG <jny5360@gmail.com>
2024-02-15 17:54:46 +0900

    123

--- NY_Git/pythonProject/240215_QUEUE/swea_15126.py
+++ NY_Git/pythonProject/240215_QUEUE/swea_15126.py
@@ -4,29 +4,25 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     cheese = list(map(int, input().split()))
     pizza_pot = [-1] * N
-    pizza_cheese = {}
-
-    # 피자번호, 치즈 매칭
-    for index, value in enumerate(cheese):
-        pizza_cheese[f'{index+1}'] = value
+    cheese_lst = [[i+1, cheese[i]] for i in range(len(cheese))]
 
     for _ in range(N):
         pizza_pot.pop()
-        pizza_pot.insert(0, cheese.pop(0))
+        pizza_pot.insert(0, cheese_lst.pop(0))
 
-    while len(pizza_pot) != 1:
-        if len(cheese) != 0:
-            if pizza_pot[0] == 0:
+    while len(pizza_pot) == 1:
+        if len(cheese_lst) != 0:
+            if pizza_pot[0][-1] == 0:
                 pizza_pot.pop(0)
-                pizza_pot.insert(0, cheese.pop(0))
+                pizza_pot.insert(0, cheese_lst.pop(0))
             else:
-                pizza_pot.append(pizza_pot.pop(0)//2)
+                pizza_pot.append([pizza_pot[0][0], pizza_pot[0][-1]//2])
+                pizza_pot.pop(0)
         else: # cheese에 값이 없을 경우
-            if pizza_pot[0] == 0:
+            if pizza_pot[0][-1] == 0:
                 pizza_pot.pop(0)
             else:
-                pizza_pot.append(pizza_pot.pop(0)//2)
+                pizza_pot.append([pizza_pot[0][0], pizza_pot[0][-1]//2])
+                pizza_pot.pop(0)
 
-
-    print(cheese)
-    print(pizza_pot)
+    print(f'#{tc} {pizza_pot[0][0]}')
--- NY_Git/pythonProject/240215_QUEUE/test.py
+++ NY_Git/pythonProject/240215_QUEUE/test.py
@@ -4,10 +4,13 @@ for tc in range(1, T+1):
     N, M = map(int, input().split())
     cheese = list(map(int, input().split()))
     pizza_pot = [-1] * N
-    pizza_cheese = {}
 
-    # 피자번호, 치즈 매칭
-    for index, value in enumerate(cheese):
-        pizza_cheese[f'{index+1}'] = value
+    cheese_lst = [[i+1, cheese[i]] for i in range(len(cheese))]
 
-print(pizza_cheese)
+    # pizza_cheese = {}
+    #
+    # # 피자번호, 치즈 매칭
+    # for index, value in enumerate(cheese):
+    #     pizza_cheese[f'{index+1}'] = value
+
+print(cheese_lst)
