1f90beac6cb705a171c0e80385895947fbbc4273
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-03-13 00:36:42 +0900

    123

--- NY_Git/code_test/baekjoon/1202_보석 도둑.py
+++ NY_Git/code_test/baekjoon/1202_보석 도둑.py
@@ -3,29 +3,38 @@
 # K개 줄에 가방의 크기 C
 
 N, K = map(int, input().split())
-dic = {}
-weight = []
-value = []
-bag = []
+dic = {}                                # 보석의 무게, 가치를 담을 딕셔너리
+value = []                              # 보석의 가치를 오름차순 정렬 할 리스트
+bag = []                                # 가방의 크기를 입력받을 리스트
 answer = 0
 
-for i in range(N):
+for i in range(N):                      # 보석의 무게, 가치를 딕셔너리에 추가
     M, V = map(int, input().split())
     dic[V] = M
-    weight.append(M)
-    value.append(V)
+    value.append(V)                     # 보석의 가치 딕셔너리 추가
 
-value.sort(reverse=True)
+value.sort(reverse=True)                # 보석의 가치 내림차순 정렬
 
-for j in range(K):
+for j in range(K):                      # 가방 리스트에 입력
     C = int(input())
     bag.append(C)
 
-print(dic)
-print(value)
+# print('dic:', dic)
+# print('value:', value)
+# print('bag:', bag)
 
-for i in range(len(bag)):
-    if bag[i] >= dic[value[i]]:
-        answer += value[i]
+bag.sort(reverse=True)
+
+
+v = 0                # 딕셔너리 중복 허용x, 같은 가치의 보석이 2개 있을 때 문제 발생
+while bag:           # 가방의 크기보다 보석의 무게가 작거나 같으면 정답에 값 추가, 가방이 빌때까지
+    if bag[0] >= dic[value[v]]:
+        answer += value[v]
+        bag.pop(0)
+        v += 1
+
+    else:
+        v += 1
+
 
 print(answer)
--- NY_Git/code_test/baekjoon/test.py
+++ NY_Git/code_test/baekjoon/test.py
@@ -1,32 +1,31 @@
-from collections import deque
-
 def dfs(c):
-    visited[c] = 1
     ans_dfs.append(c)
+    visited[c] = 1
 
     for n in adj[c]:
         if visited[n] == 0:
             dfs(n)
 
 def bfs(s):
-    q = deque()
+    q = []
     q.append(s)
 
     ans_bfs.append(s)
     visited[s] = 1
 
     while q:
-        c = q.popleft()
+        c = q.pop(0)
 
         for n in adj[c]:
             if visited[n] == 0:
-                ans_bfs.append(n)
                 q.append(n)
+                ans_bfs.append(n)
                 visited[n] = 1
 
 
 N, M, V = map(int, input().split())
 adj = [[] for _ in range(N+1)]
+
 for _ in range(M):
     v1, v2 = map(int, input().split())
     adj[v1].append(v2)
@@ -38,9 +37,10 @@ for i in adj:
 visited = [0] * (N+1)
 ans_dfs = []
 dfs(V)
-print(*ans_dfs)
 
 visited = [0] * (N+1)
 ans_bfs = []
 bfs(V)
+
+print(*ans_dfs)
 print(*ans_bfs)
