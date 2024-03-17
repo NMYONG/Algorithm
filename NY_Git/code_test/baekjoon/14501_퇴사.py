10e8f2edcea63435e0899f200e0db63d7804b282
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-03-17 22:49:20 +0900

    123

--- NY_Git/code_test/baekjoon/14501_퇴사.py
+++ NY_Git/code_test/baekjoon/14501_퇴사.py
@@ -1,16 +1,42 @@
-n = int(input())
-T, P = [0 for i in range(n+1)], [0 for i in range(n+1)]
-for i in range(n):
-    a,b = map(int, input().split())
-    T[i] = a
-    P[i] = b
+# n = int(input())
+# T, P = [0 for i in range(n+1)], [0 for i in range(n+1)]
+# for i in range(n):
+#     T[i], P[i] = map(int, input().split())
+#
+# # dp[i]는 i번째날까지 일을 했을 때, 최대값이다.
+# dp =[0 for i in range(n+1)]
+#
+# for i in range(n-2, -1, -1):      # 역순으로 진행
+#     if T[i]+i <= n:       # 날짜를 초과하지 않을 경우.
+#         dp[i] = max(P[i] + dp[i + T[i]], dp[i+1])
+#     else:                 # 날짜를 초과할 경우.
+#         dp[i] = dp[i+1]
+# print(dp[0])
 
-# dp[i]는 i번째날까지 일을 했을 때, 최대값이다.
-dp =[0 for i in range(n+1)]
 
-for i in range(len(T)-2, -1, -1):      # 역순으로 진행
-    if T[i]+i <= n:       # 날짜를 초과하지 않을 경우.
-        dp[i] = max(P[i] + dp[i + T[i]], dp[i+1])
-    else:                 # 날짜를 초과할 경우.
-        dp[i] = dp[i+1]
-print(dp[0])
+
+# 완전 탐색
+
+def dfs(idx, profit):
+    global answer
+
+    if idx >= N:                        # 종료조건 설정
+        answer = max(answer, profit)    # 모든 경우에 대해서 실행하므로 최댓값
+        return
+
+    if N >= idx + T[idx]:               # 가능한 경우 : idx는 idx+T[idx], profit은 profit+P[idx]
+        dfs(idx+T[idx], profit+P[idx])
+
+    dfs(idx+1, profit)                  # 불가능 한 경우 : idx는 +1, profit은 그대로
+
+
+N = int(input())
+T, P = [0 for _ in range(N)], [0 for _ in range(N)]
+
+for i in range(N):
+    T[i], P[i] = map(int, input().split())
+
+answer = 0
+
+dfs(0, 0)
+print(answer)
---
+++ NY_Git/code_test/baekjoon/14889_스타트와 링크.py
@@ -0,0 +1,45 @@
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+
+# min_v = 99999
+#
+# for i in range(N):
+#     for j in range(N):
+#         if i == j:
+#             continue
+#         team_s = arr[i][j] + arr[j][i]
+#         team_l = arr[N-1-i][N-1-j] + arr[N-1-j][N-1-i]
+#         gap = abs(team_s - team_l)
+#
+#         if gap == 0:
+#             print(gap)
+#             exit()
+#
+#         if min_v > gap:
+#             min_v = gap
+#
+# print(min_v)
+
+
+# 조합
+def combi(k, start_row, start_col):
+    if k == K:
+        print(path)
+        return
+
+    for i in range(start_row, len(arr)):
+        for j in range(start_col if i == start_row else 0, len(arr[0])):
+            path[k] = (i, j)
+            combi(k+1, i, j+1)
+
+arr = [[0, 1, 2, 3, 4, 5]]  # 주어진 이중 배열
+N = len(arr)  # 이중 배열의 행 수
+M = len(arr[0])  # 이중 배열의 열 수
+K = 3  # 팀을 이룰 인원 수
+
+path = [-1] * K
+combi(0, 0, 0)
+
+
+
+
