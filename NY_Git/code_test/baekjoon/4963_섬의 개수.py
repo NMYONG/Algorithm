670a329cc314446e60466e7c70344caaf63583ef
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-03-20 01:08:15 +0900

    123

--- NY_Git/code_test/baekjoon/4963_섬의 개수.py
+++ NY_Git/code_test/baekjoon/4963_섬의 개수.py
@@ -1,14 +1,23 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-dr, dc = [-1, -1, -1, 0, 0, 1, 1, 1], [-1, 0, 1, -1, 1, -1, 0, 1]
-cnt = 0
 
 # bfs
-def bfs(r, c):
+def bfs(row, col):
     q = deque()
-    q.append((r, c))
+    q.append((row, col))
+    # visited[row][col] = 1
 
     while q:
+        r, c = q.popleft()
+        for d in range(8):
+            new_r = r + dr[d]
+            new_c = c + dc[d]
+
+            if 0 <= new_r < N and 0 <= new_c <M and arr[new_r][new_c] == 1:
+                q.append((new_r, new_c))
+                arr[new_r][new_c] = 0
 
 
 # 입력
@@ -17,12 +26,15 @@ while True:
     if M == 0 and N == 0: break
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    visited = [[0] * M for _ in range(M)]
+    cnt = 0
+    dr = [-1, -1, -1, 0, 0, 1, 1, 1]
+    dc = [-1, 0, 1, -1, 1, -1, 0, 1]
+    # visited = [[0] * M for _ in range(M)]
 
     for row in range(N):
         for col in range(M):
-            if arr[row][col] == 1 and visited[row][col] == 0:
-                for d in range(8):
-                    new_r = row + dr[d]
-                    new_c = col + dc[d]
+            if arr[row][col] == 1 :
+                bfs(row, col)
+                cnt += 1
 
+    print(cnt)
