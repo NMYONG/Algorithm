4bee49ae7c4a51d109cfc2c0e7c5df7d274c3938
JiEung2 <181757@jnu.ac.kr>
2024-04-03 17:48:38 +0900

    123

--- NY_Git/Algorithm/swea/1949_등산로 조성.py
+++ NY_Git/Algorithm/swea/1949_등산로 조성.py
@@ -1,22 +1,41 @@
-from collections import deque
-import sys
+import sys, copy
 sys.stdin = open('1949_등산로 조성.txt', 'r')
 
 
-def dfs(r, c, len):
+# def dfs(r, c, len, arr):
+#     global max_len
+#
+#     visited[r][c] = 1  # 방문 표시
+#     max_len = max(max_len, len)
+#
+#     for d in range(4):
+#         nr = r + dr[d]
+#         nc = c + dc[d]
+#
+#         if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and arr[nr][nc] < arr[r][c]:
+#             dfs(nr, nc, len + 1)
+#
+#     visited[r][c] = 0  # 백트래킹
+
+def dfs(r, c, length, flag):
     global max_len
 
-    visited[r][c] = 1  # 방문 표시
-    max_len = max(max_len, len)
+    visited[r][c] = 1
+    max_len = max(max_len, length)
 
     for d in range(4):
         nr = r + dr[d]
         nc = c + dc[d]
 
-        if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc] and arr[nr][nc] < arr[r][c]:
-            dfs(nr, nc, len + 1)
+        if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]: # 범위 내에 있고 방문하지 않았다면
+            if arr[nr][nc] >= arr[r][c]:
+                for k in range(1, K+1): # 1부터 K까지 깎기
+                    arr[nr][nc] -= k
 
-    visited[r][c] = 0  # 백트래킹
+            else:
+                visited[nr][nc] = 1
+                dfs(nr, nc, length+1, True)
+
 
 
 T = int(input())
@@ -44,6 +63,8 @@ for tc in range(1, T + 1):
     max_len = 0  # 등산로의 최대길이 변수 초기화
     for row, col in highest_lst:
         visited = [[0] * N for _ in range(N)]
-        dfs(row, col, 1)
+        temp_arr = copy.deepcopy(arr) # 깊은복사한 arr에 대해서 dfs실행
+
+        dfs(row, col, 1, True)
 
     print(max_len)
