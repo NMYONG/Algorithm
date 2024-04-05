21afc22b946fb07188cdafb1f04710bb760a7d21
JiEung2 <181757@jnu.ac.kr>
2024-04-05 17:52:02 +0900

    123

--- NY_Git/Algorithm/baekjoon/3055_탈출.py
+++ NY_Git/Algorithm/baekjoon/3055_탈출.py
@@ -8,48 +8,53 @@ R행 C열
 '''
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-def dochi_bfs(row, col):
-    q = deque()
-    q.append((row, col))
-    visited[row][col] = 1 # 나중에 1 빼주기
 
+def dfs():
     while q:
         r, c = q.popleft()
+        visited[r][c] = 0
 
-        for d in range(4):
+        if maps[Dr][Dc] == 'S':  # 고슴도치가 굴에 도착하면
+            return visited[Dr][Dc] # 거리 반환
+
+        for d in range(4): # 고슴도치의 자리에서 4방향 탐색
             nr = r + dr[d]
             nc = c + dc[d]
 
-            if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
-                q.append((nr, nc))
-                visited[nr][nc] = visited[r][c] + 1
+            if 0 <= nr < R and 0 <= nc < C: # 범위 내에 있다면
+                # 현재 위치가 S이고 이동할 자리가 .이거나 D이면 (고슴도치 이동)
+                if (maps[nr][nc] == '.' or maps[nr][nc] == 'D') and maps[r][c] == 'S':
+                    visited[nr][nc] = visited[r][c] + 1
+                    maps[nr][nc] = 'S'
+                    q.append((nr, nc))
+                # 현재 위치가 *이고 이동할 자리가 .이거나 S이면 (물 이동)
+                elif (maps[nr][nc] == '.' or maps[nr][nc] == 'S') and maps[r][c] == '*':
+                    maps[nr][nc] = '*'
+                    q.append((nr, nc))
 
 
-N, M = map(int, input().split())
-maps = [list(input()) for _ in range(N)]
+R, C = map(int, input().split())
+maps = [list(map(int, input().split())) for _ in range(R)]
 
+q = deque()
+visited = [[-1] * C for _ in range(R)] # 거리를 계산할 2중배열 만들기
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
-visited = [[0] * M for _ in range(N)]
 
-gool_r = 0
-gool_c = 0
-for i in range(N):
-    for j in range(M):
-        if maps[i][j] == 'D':
-            gool_r = i
-            gool_c = j
+for r in range(R):
+    for c in range(C):
+        if maps[r][c] == 'S':
+            q.append((r, c))
+        if maps[r][c] == 'D':
+            Dr, Dc = r, c
 
-dochi_r = 0
-dochi_c = 0
-for i in range(N):
-    for j in range(M):
-        if maps[i][j] == 'S':
-            dochi_r = i
-            dochi_c = j
+for r in range(R):
+    for c in range(C):
+        if maps[r][c] == '*':
+            q.append((r, c))
 
-dochi_bfs(dochi_r, dochi_c)
 
-print(visited[gool_r][gool_c])
+# https://yunchan97.tistory.com/35
