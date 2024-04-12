fa81ab202cffa40c0ac41106165e1349762d32e7
NMYONG <jny5360@naver.com>
2024-04-12 17:43:58 +0900

    123

--- NY_Git/Algorithm/baekjoon/15657_N과 M (8).py
+++ NY_Git/Algorithm/baekjoon/15657_N과 M (8).py
@@ -1,15 +1,15 @@
-def dfs(n, idx, lst):
+def dfs(n, s, lst):
     if n == M:
         ans.append(lst)
         return
 
-    for i in range(idx, N):
+    for i in range(s, N):
         dfs(n + 1, i, lst + [num_lst[i]])
 
 N, M = map(int, input().split())
 num_lst = list(map(int, input().split()))
-
 num_lst.sort()
+
 ans = []
 
 dfs(0, 0, [])
