bc401fd78951c30de5f12538911f8ad43f473102
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-16 00:55:23 +0900

    123

--- NY_Git/Algorithm/baekjoon/14890_경사로.py
+++ NY_Git/Algorithm/baekjoon/14890_경사로.py
@@ -1,25 +1,57 @@
+# N, L = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+#
+# cnt = 0 # 등산로의 개수를 저장할 변수
+#
+# # 가로 등산로에 대해서 검사
+# for i in range(N):
+#     flag = 0
+#     cnt = 0
+#     for a in range(0, N-1): # 정방향
+#         if abs(arr[i][a] - arr[i][a+1]) > 1: # 차이가 2이상이면
+#             cnt = 0
+#             continue
+#         if abs(arr[i][a] - arr[i][a+1]) <= 1: # 차이가 1이하이면
+#             cnt += 1
+#         pass
+#
+#
+# # 세로 등산로에 대해서 검사
+# for j in range(N):
+#     for a in range(0, N): # 정방향
+#         pass
+#
 N, L = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+answer = 0
 
-cnt = 0 # 등산로의 개수를 저장할 변수
-
-# 가로 등산로에 대해서 검사
 for i in range(N):
     flag = 0
-    cnt = 0
-    for a in range(0, N-1): # 정방향
-        if abs(arr[i][a] - arr[i][a+1]) > 1: # 차이가 2이상이면
-            cnt = 0
+    for j in range(N-1):
+        # 현재 숫자와 다음 숫자가 같으면
+        if arr[i][j] == arr[i][j+1]:
             continue
-        if abs(arr[i][a] - arr[i][a+1]) <= 1: # 차이가 1이하이면
-            cnt += 1
-        pass
 
+        # 다음 숫자가 더 크고 그 차이가 1이하라면
+        elif arr[i][j] < arr[i][j+1] and (arr[i][j+1] - arr[i][j]) <= 1:
+            # L만큼 길이가 존재하는지 검사
+            try:
+                status = True # 상태를 저장할 수 있는 변수
+                # L 길이에 대해서
+                for l in range(1, L+1):
+                    if arr[i][j+1-i] == arr[i][j+1] - 1:
+                        status = True
+                    else:
+                        flag = 0
+                        break
+                if status:
+                    flag = 1
+            except IndexError: # 인덱스 에러가 나면 경사로 만들 수 없다.
+                flag = 0
 
-# 세로 등산로에 대해서 검사
-for j in range(N):
-    for a in range(0, N): # 정방향
-        pass
+        if flag:
+            answer += 1
 
-
-#
+        # 다음 숫자가 더 작고 그 차이가 1이하라면
+        elif arr[i][j] > arr[i][j+1] and (arr[i][j] - arr[i][j+1]) <= 1:
+            pass
