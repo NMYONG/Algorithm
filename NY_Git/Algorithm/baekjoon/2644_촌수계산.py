02b56b336bd3ae8265fce3760ae4f2db40786040
NMYONG <jny5360@naver.com>
2024-04-19 17:39:56 +0900

    123

--- NY_Git/Algorithm/baekjoon/2644_촌수계산.py
+++ NY_Git/Algorithm/baekjoon/2644_촌수계산.py
@@ -1,31 +1,71 @@
-def dfs(start, end):
-    global cnt
+DFS 풀이
 
-    if start == end:
-        return
+def dfs(start, cnt):
+    global flag
 
-    visited[start] = 1
+    visited[start] = True
+
+    if start == B:
+        flag = True
+        print(cnt)
 
     for c in adj[start]:
-        if not visited[c]:
-            dfs(c, end)
+        if visited[c] == False:
+            dfs(c, cnt + 1)
 
-
-    return -1
 
 N = int(input())
 A, B = map(int, input().split())
 M = int(input())
 adj = [[] for _ in range(N + 1)]
-
-for _ in range(N - 2):
+for _ in range(M):
     x, y = map(int, input().split())
     adj[x].append(y)
     adj[y].append(x)
 
-# print(adj)
-visited = [0] * (N + 1)
-cnt = 0
-dfs(A, B)
+visited = [False for _ in range(N+1)]
+flag = False
+dfs(A, 0)
 
-print(cnt)
+if flag == False:
+    print(-1)
+
+
+
+
+# BFS 풀이
+
+def bfs(start, end):
+    q = []
+    visited = [0] * (N+1)
+
+    q.append(start)
+    visited[start] = 1
+
+    while q:
+        s = q.pop(0)
+
+        if s == end:
+
+            return visited[s] - 1
+
+        for c in adj[s]:
+            if not visited[c]:
+                visited[c] = visited[s] + 1
+                q.append(c)
+
+    return -1
+
+
+N = int(input())
+A, B = map(int, input().split())
+M = int(input())
+adj = [[] for _ in range(N + 1)]
+for _ in range(M):
+    x, y = map(int, input().split())
+    adj[x].append(y)
+    adj[y].append(x)
+
+
+
+print(bfs(A, B))
