7e6d0469a24c1d41ee11b50ea7f0606652df0c49
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-22 00:43:44 +0900

    123

--- NY_Git/Algorithm/baekjoon/1991_트리 순회.py
+++ NY_Git/Algorithm/baekjoon/1991_트리 순회.py
@@ -1,10 +1,5 @@
 N = int(input())
 TREE = {}
-
-ansPreorder = []
-ansInorder = []
-ansPostorder = []
-
 for _ in range(N):
     root, left, right = input().split()
     TREE[root] = [left, right]
@@ -12,27 +7,26 @@ for _ in range(N):
 # 전위 순회
 def preOrder(root):
     if root != '.':
-        ansPreorder.append(root)
+        print(root, end='')
         preOrder(TREE[root][0])
         preOrder(TREE[root][1])
 
 def inOrder(root):
     if root != '.':
         inOrder(TREE[root][0])
-        ansInorder.append(root)
+        print(root, end='')
         inOrder(TREE[root][1])
 
 def postOrder(root):
     if root != '.':
         postOrder(TREE[root][0])
         postOrder(TREE[root][1])
-        ansPostorder.append(root)
-
-preOrder('A')
-inOrder('A')
-postOrder('A')
+        print(root, end='')
 
 print(TREE)
-print(''.join(map(str, ansPreorder)))
-print(''.join(map(str, ansInorder)))
-print(''.join(map(str, ansPostorder)))
+
+preOrder('A')
+print()
+inOrder('A')
+print()
+postOrder('A')
