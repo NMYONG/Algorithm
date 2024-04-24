ab3fe29b4c008f2f4f6c635dc836a3a82a6aee90
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-25 00:57:37 +0900

    123

--- NY_Git/Algorithm/baekjoon/11725_트리의 부모 찾기.py
+++ NY_Git/Algorithm/baekjoon/11725_트리의 부모 찾기.py
@@ -1,14 +1,24 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 N = int(input())
-adj = [[] for _ in range(N+1)]
-for _ in range(N-1):
+adj = [[] for _ in range(N + 1)]
+parent = [0] * (N + 1) # 각 노드의 부모를 기록하는 배열
+for _ in range(N - 1):
     a, b = map(int, input().split())
     adj[a].append(b)
     adj[b].append(a)
 
-print(adj)
+# print(adj)
 
-for i in range(2, N+1):
-    print(adj[i][0])
+def dfs(v):
+    for i in adj[v]:
+        if not parent[i]: # 부모를 찾지 못했다면
+            parent[i] = v # v와 연결되어 있는데 부모 배열이 0이다? => v가 부모다.
+            dfs(i)
+
+dfs(1)
+
+for j in range(2, len(parent)):
+    print(parent[j])
