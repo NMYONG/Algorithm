f423d13e58407c13609a862aac40a7d28d716810
NMYONG <jny5360@naver.com>
2024-04-26 17:47:52 +0900

    123

---
+++ NY_Git/Algorithm/baekjoon/2798_블랙잭.py
@@ -0,0 +1,25 @@
+def dfs(n, lst):
+    global max_sum
+
+    if n == 3:
+        if sum(lst) <= M:
+            if sum(lst) > max_sum :
+                max_sum = sum(lst)
+        return
+
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = 1
+            dfs(n+1, lst + [numList[i]])
+            visited[i] = 0
+
+N, M = map(int, input().split())
+numList = list(map(int, input().split()))
+
+ans = []
+visited = [0] * N
+
+max_sum  = 0
+dfs(0, [])
+
+print(max_sum)
