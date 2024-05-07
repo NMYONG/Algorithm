50b4ac5a3fc32b584cb5cea2ce94afebb477d239
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-05-07 17:04:13 +0900

    [Silver V] Title: 덩치, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,12 @@
+N = int(input())
+lst = []
+for _ in range(N):
+    height, weight = map(int, input().split())
+    lst.append((height, weight))
+
+for i in lst:
+    rank = 1
+    for j in lst:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
